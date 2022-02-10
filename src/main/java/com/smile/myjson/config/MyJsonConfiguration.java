c9e723eb4229bfe806e186672f85ec7d80b3a0b7
lion.chuang <lion.chuang@shoalter.com>
2022-02-10 10:18:25 +0800

    20220210 init

---
+++ src/main/java/com/smile/myjson/config/MyJsonConfiguration.java
@@ -0,0 +1,31 @@
+package com.smile.myjson.config;
+
+import com.smile.myjson.service.MyJsonService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 標識此類是配置類
+@ConditionalOnClass(MyJsonService.class) // 表示只有指定的class在classpath上時才能被註冊
+@EnableConfigurationProperties(MyJsonProperties.class) // 啟用@ConfigurationProperties
+public class MyJsonConfiguration {
+
+  private MyJsonProperties myJsonProperties;
+
+  // 自動注入配置類
+  public MyJsonConfiguration(MyJsonProperties myJsonProperties) {
+    this.myJsonProperties = myJsonProperties;
+  }
+
+  // 建立MyJsonService物件，注入到Spring容器中
+  @Bean
+  @ConditionalOnMissingBean(MyJsonService.class)
+  public MyJsonService myJsonService() {
+    MyJsonService myJsonService = new MyJsonService();
+    myJsonService.setPrefixName(myJsonProperties.getPrefixName());
+    myJsonService.setSuffixName(myJsonProperties.getSuffixName());
+    return myJsonService;
+  }
+}
